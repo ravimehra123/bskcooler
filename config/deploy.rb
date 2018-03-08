4a1a93025a2fb74a62b65c9ffb6eadf2e9da4440
Ravi Mehra <ravi.lib17@gmail.com>
2018-03-08 17:59:24 +0530

    Changed production env

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,6 +45,17 @@ namespace :puma do
   before :start, :make_dirs
 end
 
+before 'deploy:assets:precompile', :symlink_config_files
+
+desc "Link shared files"
+task :symlink_config_files do
+  symlinks = {
+    "#{shared_path}/config/database.yml" => "#{release_path}/config/database.yml",
+    "#{shared_path}/config/local_env.yml" => "#{release_path}/config/local_env.yml"
+  }
+  run symlinks.map{|from, to| "ln -nfs #{from} #{to}"}.join(" && ")
+end
+
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
