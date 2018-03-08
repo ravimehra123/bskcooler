7fab240f5f5fa1b262e684e9865c371f1eb25109
Ravi Mehra <ravi.lib17@gmail.com>
2018-03-08 14:34:22 +0530

    Changed production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,17 +20,17 @@ Ecomm::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
@@ -59,7 +59,7 @@ Ecomm::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
+  # config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
